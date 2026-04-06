17f3b0a9cb0ef1eec1d21e4d5c24ad64b8185e01
do-haru <doharu05@naver.com>
2026-04-06 13:57:05 +0900

    init: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/doharu/binzip_recommend/BinzipRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.doharu.binzip_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinzipRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinzipRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doharu/binzip_recommend/BinzipRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doharu.binzip_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinzipRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
